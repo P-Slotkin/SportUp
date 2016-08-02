c5764bffdd0b0873c42cd102844e41602fa791f6
Peter Slotkin <slotkin.peter@gmail.com>
2016-08-02 17:16:17 -0400

    testing heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,50 +4,50 @@ class User < ActiveRecord::Base
   after_initialize :ensure_session_token
 
   validates :password_digest, presence: true
-  validates :password, length: { minimum: 6 }, allow_nil: true 
+  validates :password, length: { minimum: 6 }, allow_nil: true
   validates :session_token, presence: true
   validates :email, presence: true, uniqueness: true
 
-  has_many :rsvps
-  has_many(
-    :comments,
-    class: "Comment",
-    primary_key: :id,
-    foreign_key: :author_id
-  )
-
-  has_many(
-    :created_events,
-    class: "Event",
-    primary_key: :id,
-    foreign_key: :creator_id
-  )
-
-  has_many(
-    :created_groups,
-    class: "Group",
-    primary_key: :id,
-    foreign_key: :creator_id
-  )
-
-  has_many(
-    :memberships,
-    class: "Member",
-    primary_key: :id,
-    foreign_key: :user_id
-  )
-
-  has_many(
-    :groups,
-    through: :memberships,
-    source: :groups
-  )
-
-  has_many(
-    :rsvped_events,
-    through: :rsvps,
-    source: :events
-  )
+  # has_many :rsvps
+  # has_many(
+  #   :comments,
+  #   class_name: "Comment",
+  #   primary_key: :id,
+  #   foreign_key: :author_id
+  # )
+  #
+  # has_many(
+  #   :created_events,
+  #   class_name: "Event",
+  #   primary_key: :id,
+  #   foreign_key: :creator_id
+  # )
+  #
+  # has_many(
+  #   :created_groups,
+  #   class_name: "Group",
+  #   primary_key: :id,
+  #   foreign_key: :creator_id
+  # )
+  #
+  # has_many(
+  #   :memberships,
+  #   class_name: "Member",
+  #   primary_key: :id,
+  #   foreign_key: :user_id
+  # )
+  #
+  # has_many(
+  #   :groups,
+  #   through: :memberships,
+  #   source: :groups
+  # )
+  #
+  # has_many(
+  #   :rsvped_events,
+  #   through: :rsvps,
+  #   source: :events
+  # )
 
   def self.find_by_credentials(email, password)
     user = User.find_by(email: email)
